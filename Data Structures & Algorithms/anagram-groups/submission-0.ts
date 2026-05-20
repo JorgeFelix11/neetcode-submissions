5bfca5969f67355a147910cbd26bc79bdc72d9f8
JorgeFelix11 <38844068+JorgeFelix11@users.noreply.github.com>
2026-05-19 22:20:37 -0600

    Add: anagram-groups - submission-0

---
+++ Data Structures & Algorithms/anagram-groups/submission-0.ts
@@ -0,0 +1,26 @@
+class Solution {
+    /**
+     * @param {string[]} strs
+     * @return {string[][]}
+     */
+    groupAnagrams(strs: string[]): string[][] {
+        let res: string[][]
+
+        let map = new Map<string, string[]>()
+
+        for(let i = 0; i < strs.length; i++) {
+            let arr = new Uint8Array(26)
+            for(let j = 0; j < strs[i].length; j++) {
+                let letterIdx = this.getLetterIndex(strs[i][j])
+                arr[letterIdx]++ 
+            }
+            map.set(arr.toString(), [...(map.get(arr.toString()) || []),strs[i]])
+        }
+        console.log(map)
+        return [...map.values()]
+    }
+
+    getLetterIndex(letter) {
+        return letter.toUpperCase().charCodeAt(0) - 65
+    }
+}
